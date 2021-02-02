ed5e72bf139c7efaf5bc1113e24e5c31e4dffdf2
mezage <mezage@uci.edu>
2021-02-01 21:57:12 -0800

    Included test template for weather effecting user

--- Essentials/src/test/java/com/earth2me/essentials/WeatherTest.java
+++ Essentials/src/test/java/com/earth2me/essentials/WeatherTest.java
@@ -1,17 +1,23 @@
 package com.earth2me.essentials;
 
+import com.earth2me.essentials.commands.Commandpweather;
+import com.earth2me.essentials.commands.Commandweather;
 import junit.framework.TestCase;
+import org.bukkit.WeatherType;
 import org.bukkit.World;
 import org.bukkit.plugin.InvalidDescriptionException;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class WeatherTest extends TestCase {
-    private final OfflinePlayer base1;
+    private final OfflinePlayer base1, base2;
     private final Essentials ess;
     private final FakeServer server;
+    private final Commandpweather playerW;
+    private final Commandweather serverW;
 
-    //no clue where this testname is established
+    //no clue where this testName is established
     public WeatherTest(final String testName){
         super(testName);
         server = new FakeServer();
@@ -25,11 +31,34 @@ public class WeatherTest extends TestCase {
             fail("IOException");
         }
         base1 = server.createPlayer("testPlayer1");
+        base2 = server.createPlayer("testPlayer2");
         server.addPlayer(base1);
+        server.addPlayer(base2);
         ess.getUser(base1);
+        //ess.getUser(base2);   //possibly not needed, only returns User?
+
+        playerW = new Commandpweather();
+        serverW = new Commandweather();
     }
 
-    public void userWeather(){}
+    public void userWeather(){
+        final User user = ess.getUser(base1);
 
-    public void serverWeather(){}
+        //structure of call /pweather get *, or /pweather <weather condition> <target user>
+        try{
+            playerW.run(server, null, null, new String[]{"sun", "testPlayer1"});
+            //need to change sender from null to player identifier?
+
+            assertEquals(user.getBase().getPlayerWeather().toString().toLowerCase(Locale.ENGLISH), WeatherType.CLEAR.toString());
+
+            //if works, repeat for all cases
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void serverWeather(){
+
+    }
 }
